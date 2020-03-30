5921ad19c3c50f55afc000bdd208dcc324ce80b5
Adam Rafiandri <rafiandria23@gmail.com>
2020-03-30 11:05:50 +0700

    Created React project using CRA.

--- sunday-kanban-server/helpers/generate-token.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-const jwt = require("jsonwebtoken");
-
-module.exports = (payload) => {
-  return jwt.sign(payload, process.env.JWT_SECRET_KEY);
-};
--- sunday-kanban-server/routes/category.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const categoryRouter = require("express").Router();
-const CategoryController = require("../controllers/category");
-
-categoryRouter.get("/", CategoryController.findAll);
-
-module.exports = categoryRouter;
